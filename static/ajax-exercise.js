b0601929355d1b8606b791e724c07413440476c5
Hackbright Student <info@hackbrightacademy.com>
2015-02-13 15:56:56 -0800

    completed exercise

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -1,13 +1,11 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
-    // TODO: get the fortune and show it in the fortune-text div
+    // Get the fortune and show it in the fortune-text div
+    $("#fortune-text").load("/fortune");
 }
 
 $('#get-fortune-button').on('click', showFortune);
-
-
-
 
 
 // PART 2: SHOW WEATHER
@@ -16,13 +14,12 @@ function showWeather(evt) {
     evt.preventDefault();
     var url = "/weather?zipcode=" + $("#zipcode-field").val();
     // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get(url, function(result) {
+        $("#weather-info").append(result.forecast,"<br>", result.temp);
+    });
 }
 
 $("#weather-form").on('submit', showWeather);
-
-
-
-
 
 
 // PART 3: ORDER MELONS
@@ -33,8 +30,15 @@ function orderMelons(evt) {
     $.post("/order-melons",
         $('#order-form').serialize(),
         function (result) {
-            // TODO: show the result message after your form
-            // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+            $("#order-status").text(result.msg);
+
+            if(result.code === "ERROR") {
+                $("#order-status").addClass("order-error");
+            }
+
+            if(result.code !== "ERROR") {
+                $("#order-status").removeClass("order-error");
+            }
         }
     );
 }
